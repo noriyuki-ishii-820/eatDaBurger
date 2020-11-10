89e030cdc7daedff8508ff3c65b72db65ada14c1
Noriyuki Ishii <nori-ktcc820@hotmail.co.jp>
2020-11-10 18:07:18 +1100

    basic codes on serverjs done

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,23 @@
-setup the code to connect Node to MySQL.
+// Set up MySQL connection.
 
-   * Export the connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Kreva141!",
+  database: "burgers_db",
+});
+
+// Make connection.
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-// add require of the npm packages
+// Dependencies
+
+var express = require("express");
+var app = express();
+
+// Handlebars
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+
+var routes = require("./config/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
